67b649499b20f1ad922b80022fad72de2190bb21
Candace Buchanan <Candace@Candaces-MacBook-Pro.local>
2018-03-30 13:21:41 -0400

    everything is working except sound effects.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,7 +18,8 @@ window.onload = function () {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
         
         //play sound effect when player gets a win
-        var audio = new Audio('assets/images/588688352.mp4');
+        var winnerAudio = document.createElement('audio');
+            winnerAudio.src = 'assets/images/twinkle.mp3';
 
         // Randomly chooses a choice from the options array. This is the Computer's guess.
         var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -26,12 +27,13 @@ window.onload = function () {
         //This logic determines the outcome of the game (win/loss), and increments the appropriate number    
         if (/^[a-zA-Z]+$/.test(userGuess) && letters.indexOf(userGuess) == -1) {
             letters.push(userGuess);
+            
             if (userGuess === computerGuess) {
                 wins++;
                 alert('Way to go! You Won!');
                 guesses = 9; //resets guesses to 9
                 letters.length = 0; //removes previously guessed letters
-                audio.play();
+                winnerAudio.play();
             }
             else if (guesses === 0) {
                 losses++;
